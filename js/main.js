9fee170f030fb1f9be7ac0306df06428cc31c62a
Juris Zemitis <juris.zemitis@gmail.com>
2022-11-24 16:07:17 +0200

    mobile layout and validation done

---
+++ js/main.js
@@ -0,0 +1,40 @@
+
+const email = document.getElementById('email-input');
+const submit = document.body.getElementsByClassName('button')[0];
+const errorMsg = document.body.getElementsByClassName('error-msg')[0];
+const emailRegEx = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+
+console.log(errorMsg);
+
+let validateEmail = () => {
+    let inputEmail = email.value;
+    errorMsg.innerHTML = '';
+    errorMsg.style.color = '#FB3E3E'
+    console.log(email.value);
+    if (inputEmail === '') {
+        errorMsg.innerHTML = 'Oops! Please add your email';
+    }
+    if (!emailRegEx.test(inputEmail)){
+        errorMsg.innerHTML = 'Oops! Please check your email';
+    } else {
+        errorMsg.style.color = '#54E6AF'
+        errorMsg.innerHTML = 'Thank You!';
+    }
+    email.style.backgroundColor = `rgb(${rndColor()}, ${rndColor()},${rndColor()})`;
+    
+}
+
+let rndColor = () => {
+    return Math.floor(Math.random() * 256)
+}
+
+submit.onclick = validateEmail;
+
+// Your users should be able to:
+
+// - View the optimal layout depending on their device's screen size
+// - See hover states for interactive elements
+// - Receive an error message when the form is submitted if:
+//   - The `Email address` field is empty should show "Oops! Please add your email"
+//   - The email is not formatted correctly should show "Oops! Please check your email"
